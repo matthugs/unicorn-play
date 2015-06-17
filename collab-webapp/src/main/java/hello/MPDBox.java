25cb706168458ff03dde5a7bbfde8bb5175798ee
wbaldwin6 <wbaldwin6@gatech.edu>
2015-06-17 18:44:54 -0400

    Added MPDBox.java
    I looked and could not find what MPD actually does for hashing in the
    API (though I was likely looking in the wrong place), but the general
    idea is hopefully clear.

---
+++ collab-webapp/src/main/java/hello/MPDBox.java
@@ -0,0 +1,26 @@
+package hello;
+
+public interface MPDBox {
+	/*
+	 * Takes in a hash value and plays the song in question.
+	 * I'm not actually sure if the hash is a String, Emily may edit this to the appropriate type as she sees fit.
+	 * What is important is that it searches for a song with the appropriate hash, puts it in the front of the playlist, and then plays it.
+	 * It works like the currently existing addSong except that it can be more appropriately used by the frontend.
+	 */
+	public void playFromHash(String hash);
+	
+	/*
+	 * These are things that the player should probably have and already does.
+	 * I'll deal with querying features in the moderator's interface.
+	 */
+	
+	public void play();
+	
+	public void pause();
+	
+	public void stop();
+	
+	public void next();
+	
+	public void previous();
+}
