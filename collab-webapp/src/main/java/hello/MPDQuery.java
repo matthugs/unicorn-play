b83390016155c79d3d8d72de763effd9644b5e26
wbaldwin6 <wbaldwin6@gatech.edu>
2015-06-26 08:07:53 -0400

    Made modifications to MPDQuery
    I'm almost certain this will cause some conflict, but in general it's
    just working on making it provide them as a set of JSON objects instead
    of a list of strings, so the bot can actually use them.

--- collab-webapp/src/main/java/hello/MPDQuery.java
+++ collab-webapp/src/main/java/hello/MPDQuery.java
@@ -2,58 +2,65 @@ package hello;
 
 import org.bff.javampd.*;
 import org.bff.javampd.objects.*;
+
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 public class MPDQuery implements Query{
 
 	private MPDDatabase database;
-	private HashTable<String, MPDSong> hashTable;
+	private Hashtable<String, MPDSong> hashTable;
 
 	public MPDQuery(MPDDatabase database) {
 		this.database = database;
-		hashTable = new HashTable<String, MPDSong>();
+		hashTable = new Hashtable<String, MPDSong>();
 		ArrayList<MPDSong> allSongList = database.listAllSongs();
 		for(MPDSong song : allSongList) {
 			hashTable.put(hash(song), song);
 		}
 	}
 
-	public List<String> searchAny(String criteria){
+	public Map<String, Object> searchAny(String criteria){
 		return songListToJSONList(database.findAny(criteria));
 	}
 
-	public List<String> searchArtist(String artist){
+	public Map<String, Object> searchArtist(String artist){
 		return songListToJSONList(database.findArtist(artist));
 	}
 
-	public List<String> searchGenre(String genre){
+	public Map<String, Object> searchGenre(String genre){
 		return songListToJSONList(database.findGenre(genre));
 	}
 
-	public List<String> searchAlbum(String album){
+	public Map<String, Object> searchAlbum(String album){
 		return songListToJSONList(database.findAlbum(album));
 	}
 
-	public List<String> searchSong(String song){
+	public Map<String, Object> searchSong(String song){
 		return songListToJSONList(database.findTitle(song));
 	}
 
-	public List<String> searchYear(String year) {
-		return songListToJSONList(database.findYear(year));
-	}
-
-	private String mpdSongToJSON(MPDSong mpdsong){
+	private Map<String, Object> mpdSongToJSON(MPDSong mpdsong){
 		//TODO: FIX ME!!!!!!
 		//Should include the hash for this song: method is below.
 		return "";
 	}
 
-	private List<String> songListToJSONList(Collection<MPDSong> songList) {
-		List<String> ret = new ArrayList<String>();
+	private Map<String, Object> songListToJSONList(Collection<MPDSong> songList) {
+		/*List<String> ret = new ArrayList<String>();
 		for(MPDSong song : songList) {
 			ret.add(mpdSongToJSON(song));
+		}
+		return ret;*/
+		Map<String, Object> ret = new HashMap<String, Object>();
+		for(MPDSong song : songList) {
+			//the value should be a JSON object with the same data as the frontend list.
+			ret.put(song.getName(), mpdSongToJSON(song));
+			//may need to figure out a different key.
 		}
 		return ret;
 	}
