e49e0d985e3c05b0577aa240c8a3ed308486143d
tzhang85 <tzhang85@gatech.edu>
2015-07-06 13:11:05 -0400

    need test for display songs in lib by song page

--- collab-webapp/src/main/webapp/library/library.js
+++ collab-webapp/src/main/webapp/library/library.js
@@ -1,21 +1,21 @@
 define(
   [
-    "angular",
-    "angular-route",
-    "collab/collabInterfaceService"
+  "angular",
+  "angular-route",
+  "collab/collabInterfaceService"
   ],
   function(angular, ngRoute) {
     var app = angular.module("coPlaylist.library",
-                             ["coPlaylist.collabInterface"]);
+     ["coPlaylist.collabInterface"]);
     app.config(["$routeProvider",
-               function($routeProvider) {
-                 
-			$routeProvider
-			.when( '/library', { templateUrl: 'library/library.tmp.html', controller: 'libraryCtr' } )
-			.when( '/libBySong', { templateUrl: 'library/song.tmp.html', controller: 'libraryCtr' } )
-			.when( '/libByAlbum', { templateUrl: 'library/album.tmp.html', controller: 'libraryCtr' } )
-			.when( '/libByArtist', { templateUrl: 'library/artist.tmp.html' , controller: 'libraryCtr' } )
-    }]);
+     function($routeProvider) {
+
+       $routeProvider
+       .when( '/library', { templateUrl: 'library/library.tmp.html', controller: 'libraryCtr' } )
+       .when( '/libBySong', { templateUrl: 'library/song.tmp.html', controller: 'libraryCtr' } )
+       .when( '/libByAlbum', { templateUrl: 'library/album.tmp.html', controller: 'libraryCtr' } )
+       .when( '/libByArtist', { templateUrl: 'library/artist.tmp.html' , controller: 'libraryCtr' } )
+     }]);
     app.controller("libraryCtr", [
       "collabInterface",
       "$scope",
@@ -23,60 +23,52 @@ define(
       function(collab, $scope, $http) {
 
         var lib=[
-          {"song":"Sugar", "singer":"Maroon5"},
-          {"song":"My Bloody Valentine", "singer":"Tata Young"},
-          {"song":"Animal", "singer":"Maron5"},
-          {"song":"One More Night", "singer":"Maroon5"}
+        {"song":"Sugar", "singer":"Maroon5"},
+        {"song":"My Bloody Valentine", "singer":"Tata Young"},
+        {"song":"Animal", "singer":"Maron5"},
+        {"song":"One More Night11", "singer":"Maroon5"}
         ];
 
 
-        //loading library
-        //$http.get('lib.json').
-        //success(function(data, status, headers, config) {
+        $scope.lib = lib;
         collab.postService("query",{query: $scope.searchArtistText},
-                             function(args) {
-                               console.log(args.value.list);
-                               $scope.lib = args.value.list;
-                                console.log("find libray");
-                             });
-        //	}).
-        //	error(function(data, status, headers, config) {
-        //		console.log("loading data error, lib.json");
-        //	});
+         function(args) {
+          console.log("get lib from service");
+           console.log(args.value.list);
+           $scope.lib = args.value.list;
+         });
 
         $scope.searchArtistText = "search artist";
         $scope.searchArtist = function() {
           console.log("about to search artist " + $scope.searchArtistText);
+          $scope.lib = [];
           collab.postService("query",{query: $scope.searchArtistText},
-                             function(args) {
-                               console.log(args.value.list);
-                               $scope.lib = args.value.list;
-                             });
+           function(args) {
+             console.log(args.value.list);
+             $scope.lib = args.value.list;
+           });
 
         }
 
 
         $scope.$watchCollection( function() {return $scope.lib},
-                                function(newValue, oldValue){
-                                  if (typeof newValue !== 'undefined') {
-                                    $scope.lib = newValue;
-                                    console.log('updating lib list');
-                                  }
-                                }
-                               );
-                               $scope.addSong = function(song, singer){
-                                 //	var newSong = function() {
-                                 //		this.song = song;
-                                 //		this.singer = singer;
-                                 //	};
-                                 playlist.push({song : song, singer : singer});
-                                 //placeholder for send notification to Bot
-                                 //about the changes
-                                 $scope.playlist = playlist;
-                                 console.log("add new song locally : " + song +
-                                             " " + singer);
-                                 collab.sendSync("listChange", $scope.playlist);
-                               };
-      }]);
-  }
+          function(newValue, oldValue){
+            console.log("detect change in scope lib");
+            if (typeof newValue !== 'undefined') {
+              $scope.lib = newValue;
+              console.log('updating lib list');
+            }
+          }
+          );
+        $scope.addSong = function(song, singer){
+
+          $scope.playlist.push({song : song, singer : singer});
+
+         $scope.playlist = playlist;
+         console.log("add new song locally : " + song +
+           " " + singer);
+         collab.sendSync("listChange", $scope.playlist);
+       };
+     }]);
+}
 );
