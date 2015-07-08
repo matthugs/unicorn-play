999e5edcabf1e3c3540a5b0e5b0292cb10a7b59f
tzhang85 <tzhang85@gatech.edu>
2015-07-07 22:24:03 -0400

    seperate libCtr to three controller - songCtr albumCtr and artistCtr

--- collab-webapp/src/main/webapp/library/library.js
+++ collab-webapp/src/main/webapp/library/library.js
@@ -11,40 +11,87 @@ define(
      function($routeProvider) {
 
        $routeProvider
-       .when( '/library', { templateUrl: 'library/library.tmp.html', controller: 'libraryCtr' } )
-       .when( '/libBySong', { templateUrl: 'library/song.tmp.html', controller: 'libraryCtr' } )
-       .when( '/libByAlbum', { templateUrl: 'library/album.tmp.html', controller: 'libraryCtr' } )
-       .when( '/libByArtist', { templateUrl: 'library/artist.tmp.html' , controller: 'libraryCtr' } )
+       .when( '/library', { templateUrl: 'library/library.tmp.html'} )
+       .when( '/libBySong', { templateUrl: 'library/song.tmp.html', controller: 'songCtr' } )
+       .when( '/libByAlbum', { templateUrl: 'library/album.tmp.html', controller: 'albumCtr' } )
+       .when( '/libByArtist', { templateUrl: 'library/artist.tmp.html' , controller: 'artistCtr' } )
      }]);
-    app.controller("libraryCtr", [
+    
+
+    app.controller("songCtr", [
       "collabInterface",
       "$scope",
       "$http",
       function(collab, $scope, $http) {
 
-        var lib=[
-        {"song":"Sugar", "singer":"Maroon5"},
-        {"song":"My Bloody Valentine", "singer":"Tata Young"},
-        {"song":"Animal", "singer":"Maron5"},
-        {"song":"One More Night11", "singer":"Maroon5"}
-        ];
-
-
-        $scope.lib = lib;
-        //function of get lib list from service
         collab.postService("query",{query: $scope.searchArtistText},
          function(args) {
           console.log("get lib from service");
-           console.log(args.value.list);
+          console.log(args);
            $scope.lib = args.value.list;
+           $scope.$apply();
+         });
+
+        $scope.$watchCollection( "lib",
+          function(newValue, oldValue){
+            console.log("detect change in scope lib");
+            if (typeof newValue !== 'undefined') {
+              $scope.lib = newValue;
+              console.log('updating lib list');
+              console.log(newValue);
+            }
+          }
+          );
+     }]);
+
+    app.controller("albumCtr", [
+      "collabInterface",
+      "$scope",
+      "$http",
+      function(collab, $scope, $http) {
+
+        collab.postService("query",{query: $scope.searchArtistText},
+         function(args) {
+          console.log("get lib from service");
+          console.log(args);
+           $scope.lib = args.value.list;
+           $scope.$apply();
+         });
+
+        $scope.$watchCollection( "lib",
+          function(newValue, oldValue){
+            console.log("detect change in scope lib");
+            if (typeof newValue !== 'undefined') {
+              $scope.lib = newValue;
+              console.log('updating lib list');
+              console.log(newValue);
+            }
+          }
+          );
+     }]);
+
+
+    app.controller("artistCtr", [
+      "collabInterface",
+      "$scope",
+      "$http",
+      function(collab, $scope, $http) {
+
+        collab.postService("query",{query: $scope.searchArtistText},
+         function(args) {
+          console.log("get lib from service");
+          console.log(args);
+           $scope.lib = args.value.list;
+           $scope.$apply();
          });
 
         $scope.searchArtistText = "search artist";
         $scope.searchArtist = function() {
           console.log("about to search artist " + $scope.searchArtistText);
-          $scope.lib = [];
+         // $scope.lib = [];
           collab.postService("query",{query: $scope.searchArtistText},
            function(args) {
+            console.log("get artist list from server");
              console.log(args.value.list);
              $scope.lib = args.value.list;
            });
@@ -52,15 +99,17 @@ define(
         }
 
 
-        $scope.$watchCollection( function() {return $scope.lib},
+        $scope.$watchCollection( "lib",
           function(newValue, oldValue){
             console.log("detect change in scope lib");
             if (typeof newValue !== 'undefined') {
               $scope.lib = newValue;
               console.log('updating lib list');
+              console.log(newValue);
             }
           }
           );
+
         $scope.addSong = function(song, singer){
 
           $scope.playlist.push({song : song, singer : singer});
