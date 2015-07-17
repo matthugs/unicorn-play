dcbdd96ee9a77ef062e7b6bde4e3ced740132478
Emily Curtin <emily@framebit.org>
2015-07-17 17:32:32 -0400

    added a main class to test backend stuff

---
+++ collab-webapp/src/main/java/temp/ChangeOverTest.java
@@ -0,0 +1,15 @@
+package temp;
+
+import moderator.PlaylistStateManager;
+
+public class ChangeOverTest {
+	
+	//PlaylistStateManager stateManager = new PlaylistStateManager(null);
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Howdy world");
+	}
+
+}
